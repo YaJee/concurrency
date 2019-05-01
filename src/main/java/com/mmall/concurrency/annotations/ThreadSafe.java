e5c7c3fa495ff8e6b6769011b39800082c5eb865
yajie_china <f9e_100006325238_yajie@git.cloud.tencent.com>
2019-05-01 16:46:48 +0800

    add annotations

---
+++ src/main/java/com/mmall/concurrency/annotations/ThreadSafe.java
@@ -0,0 +1,15 @@
+package com.mmall.concurrency.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 用来标记【线程安全】的类或写法
+ */
+@Target(ElementType.TYPE)               //注解作用的目标，ElementType.TYPE：对类做注解
+@Retention(RetentionPolicy.SOURCE)      //注解的范围，SOURCE：编译时被忽略，仅起代码提醒功能
+public @interface ThreadSafe {
+    String value()default "";
+}
